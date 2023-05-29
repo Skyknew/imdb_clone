efcc638bfc7e96442b8eb111d932c587cf6e163a
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-05-29 14:11:11 +0545

    create the header section

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head () {
+    return (
+     <>
+         <title>IMDB Clone</title>
+         <meta content="width=device-width, initial-scale=1" name="viewport" />
+         <meta name="description" content="Google Clone created by Next js 13" />
+         <link rel="icon" href="/music.ico" />
+     </>
+     )
+  }
+  
