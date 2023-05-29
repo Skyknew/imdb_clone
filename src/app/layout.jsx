ac18eba74b6191facdb171d243a426aa7db0937a
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-05-29 16:30:07 +0545

    add dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
+import Header from '@/components/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Providers from './Providers'
 
 export const metadata = {
   title: 'IMDB ',
@@ -11,7 +10,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+
+        {/* Header */}
+        <Header/>
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+
+        {children}
+
+        </Providers>
+        
+      </body>
     </html>
   )
 }
